9e7e5b9fe7401b690f9b7365e8a0f9d2331a66df
KeithLerner <46833242+KeithLerner@users.noreply.github.com>
2025-05-22 11:05:34 -0400

    Updated hello world into raylib 3d example

--- HelloWorld_CPP.cpp
+++ HelloWorld_CPP.cpp
@@ -3,36 +3,69 @@
 
 int main(int argc, char* argv[])
 {
-    // Phase 1 output (console output stream)
-    //std::cout << "Hello, World!" << std::endl;
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    // Phase 2 output (raylib window)
-    int screenWidth = 500;
-    int screenHeight = 400;
-    //const char[] text = "Hello, World!"; 
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
 
-    InitWindow(screenWidth, screenHeight, "Hello, World!");
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = Vector3{ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = Vector3{ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
-    SetTargetFPS(144);
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
 
-    // Main raylib update loop
-    while (!WindowShouldClose()) // Detects window close button and ESC key
+    DisableCursor();                    // Limit cursor to relative movement inside the window
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
     {
-        // Update variables
-        
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera, CAMERA_FREE);
 
-        // Draw to window
+        if (IsKeyPressed('Z')) camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(Color{255, 255, 255, 255});
+        ClearBackground(RAYWHITE);
 
-        DrawText("Hello, World!", 250, 200, 20, Color{0, 0, 0, 255});
-        
+        BeginMode3D(camera);
+
+        DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+        DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+        DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawRectangle(10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(10, 10, 320, 93, BLUE);
+
+        DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+        DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+        DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+        DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
+
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    // De-initialize window
-    CloseWindow();
-    
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
     return 0;
 }
