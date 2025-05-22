5ffc038eee92f98b9cf6b1c022efb33d0e15d7bf
KeithLerner <46833242+KeithLerner@users.noreply.github.com>
2025-05-22 10:56:00 -0400

    Raylib working

--- HelloWorld_CPP.cpp
+++ HelloWorld_CPP.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include "C:\raylib\raylib\src\raylib.h"
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Phase 1 output (console output stream)
+    //std::cout << "Hello, World!" << std::endl;
+
+    // Phase 2 output (raylib window)
+    int screenWidth = 500;
+    int screenHeight = 400;
+    //const char[] text = "Hello, World!"; 
+
+    InitWindow(screenWidth, screenHeight, "Hello, World!");
+
+    SetTargetFPS(144);
+
+    // Main raylib update loop
+    while (!WindowShouldClose()) // Detects window close button and ESC key
+    {
+        // Update variables
+        
+
+        // Draw to window
+        BeginDrawing();
+
+        ClearBackground(Color{255, 255, 255, 255});
+
+        DrawText("Hello, World!", 250, 200, 20, Color{0, 0, 0, 255});
+        
+        EndDrawing();
+    }
+
+    // De-initialize window
+    CloseWindow();
+    
     return 0;
 }
