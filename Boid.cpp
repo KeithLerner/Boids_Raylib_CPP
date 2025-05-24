2adf494cb0daed1d41b81fec1fa227c1b2647ef0
KeithLerner <46833242+KeithLerner@users.noreply.github.com>
2025-05-24 16:29:32 -0400

    Better tweaked boid values

--- Boid.cpp
+++ Boid.cpp
@@ -1,9 +1,9 @@
 #include "Boid.h"
 
 float Boid::maxSpeed = 16.0f;
-float Boid::alignmentWeight = 6.0f;
+float Boid::alignmentWeight = 3.0f;
 float Boid::cohesionWeight= 1.0f;
-float Boid::separationWeight= 3.0f;
-float Boid::avoidEdgesWeight = 12.0f;
+float Boid::separationWeight= 2.0f;
+float Boid::avoidEdgesWeight = 2.0f;
 float Boid::separationDistance = 12.0f;
-float Boid::senseDistance = 64.0f;
+float Boid::senseDistance = 32.0f;
